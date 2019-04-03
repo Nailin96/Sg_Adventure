18e33fe2e8528293106bfa2528247d5ed585c554
Nailinhatecoding <zhaonailin@hotmail.com>
2019-04-03 22:21:48 +0800

    nailn's commit

---
+++ 12 Chunk the Design/localscript.js
@@ -0,0 +1,67 @@
+/* Function for logic designer
+********************************************/
+function ld_in(){
+  var a = document.getElementById("R1C1");
+  var b = document.getElementById("R1C2");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+}
+function ld_out(){
+  var a = document.getElementById("R1C1");
+  var b = document.getElementById("R1C2");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+}
+
+/* Function for Game 2
+********************************************/
+function ad_in(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R2C2");
+  var c = document.getElementById("R5C2");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+}
+function ad_out(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R2C2");
+  var c = document.getElementById("R5C2");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";  
+}
+
+/* Function for Game 3
+********************************************/
+function bd_in(){
+  var a = document.getElementById("R3C1");
+  var b = document.getElementById("R3C2");
+  var c = document.getElementById("R6C2");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+}
+function bd_out(){
+  var a = document.getElementById("R3C1");
+  var b = document.getElementById("R3C2");
+  var c = document.getElementById("R6C2");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";
+}
+
+/* Function for Game 4
+********************************************/
+function cd_in(){
+  var a = document.getElementById("R4C1");
+  var b = document.getElementById("R4C2");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+}
+function cd_out(){
+  var a = document.getElementById("R4C1");
+  var b = document.getElementById("R4C2");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+}
