5c02b547ba3fbc986c5f34ac0ef87dbbff3f11e6
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2019-04-03 17:11:08 +0800

    included js and css file

---
+++ 06 Design Alternatives/localscript.js
@@ -0,0 +1,114 @@
+/* Function for Game 1
+********************************************/
+function G1_in(){
+  var a = document.getElementById("R3C1");
+  var b = document.getElementById("R1C2");
+  var c = document.getElementById("R3C3");
+  var d = document.getElementById("R5C4");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+  d.style.background = "#b0acff";
+}
+function G1_out(){
+  var a = document.getElementById("R3C1");
+  var b = document.getElementById("R1C2");
+  var c = document.getElementById("R3C3");
+  var d = document.getElementById("R5C4");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";
+  d.style.background = "#bedfff";
+}
+
+/* Function for Game 2
+********************************************/
+function G2_in(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R1C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R4C4");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+  d.style.background = "#b0acff";
+}
+function G2_out(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R1C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R4C4");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";
+  d.style.background = "#bedfff";
+}
+
+/* Function for Game 3
+********************************************/
+function G3_in(){
+  var a = document.getElementById("R1C1");
+  var b = document.getElementById("R2C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R3C4");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+  d.style.background = "#b0acff";
+}
+function G3_out(){
+  var a = document.getElementById("R1C1");
+  var b = document.getElementById("R2C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R3C4");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";
+  d.style.background = "#bedfff";
+}
+
+/* Function for Game 4
+********************************************/
+function G4_in(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R3C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R2C4");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+  d.style.background = "#b0acff";
+}
+function G4_out(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R3C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R2C4");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";
+  d.style.background = "#bedfff";
+}
+
+/* Function for Game 5
+********************************************/
+function G5_in(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R3C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R1C4");
+  a.style.background = "#b0acff";
+  b.style.background = "#b0acff";
+  c.style.background = "#b0acff";
+  d.style.background = "#b0acff";
+}
+function G5_out(){
+  var a = document.getElementById("R2C1");
+  var b = document.getElementById("R3C2");
+  var c = document.getElementById("R5C3");
+  var d = document.getElementById("R1C4");
+  a.style.background = "#bedfff";
+  b.style.background = "#bedfff";
+  c.style.background = "#bedfff";
+  d.style.background = "#bedfff";
+}
